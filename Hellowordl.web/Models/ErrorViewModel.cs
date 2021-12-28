9138bc701369e5028df9e27d1b4834928ada15cd
simha <simhame345@gmail.com>
2021-12-28 17:25:07 +0530

    first commit

---
+++ Hellowordl.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Hellowordl.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
